58c80f02a2611546ace2d58f69b7f0c70b15cfdb
Mahmoud Elkarargy <mahmoud.elkarargy@gmail.com>
2019-05-08 15:54:37 +0200

    MainMenu
    @kareemabdellah edit the rest buttons

--- src/View/ViewManger.java
+++ src/View/ViewManger.java
@@ -1,93 +1,77 @@
 package View;
 
-import java.awt.*;
 import java.util.List;
-import java.util.ArrayList;
 
 
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.effect.DropShadow;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
-import View.*;
-import View.fruitNinjaButton;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 
 public class ViewManger {
     private AnchorPane mainpane ;
     private Scene mainscene;
     private Stage mainstage;
+    private final static int GAMEWIDTH = 1024;
+    private final static int GAMEHIGHT = 700;
 
-    private final static int menu_button_start_x= 100;
-    private final static int menu_button_start_y= 150;
-
-    List<fruitNinjaButton> menubuttons ;
+    ImageView classicButton, arcadeButton, GameZoneButton, closeButton, helpButton;
 
     public ViewManger(){
         mainpane=new AnchorPane();
         mainscene= new Scene(mainpane, 1024,700);
         mainstage= new Stage();
         mainstage.setScene(mainscene);
-        createBackground();
         createButton();
+        createBackground();
+        creatLogo();
     }
 
     public Stage getMainstage() {
         return mainstage;
     }
-    private void addMenuButtons(fruitNinjaButton button){
-      //  button.setLayoutX(menu_button_start_x);
-        //button.setLayoutY(menu_button_start_y + menubuttons.size()*100);
-        //menubuttons.add(button);
-        mainpane.getChildren().add(button);
-
-    }
 
     private  void createButton(){
-
         createClassicbutton();
-        createArcadebutton();
-        createGameZonesbutton();
-        createHelpbutton();
-        createClosebutton();
-
+//        createArcadebutton();
+//        createGameZonesbutton();
+//        createHelpbutton();
+//        createClosebutton();
     }
 
     private void createClassicbutton(){
-        fruitNinjaButton classic_button = new fruitNinjaButton("Classsic ");
-        addMenuButtons(classic_button);
-
-    } private void createArcadebutton(){
-        fruitNinjaButton arcade_button = new fruitNinjaButton("Arcade");
-        addMenuButtons(arcade_button);
-
-    } private void createGameZonesbutton(){
-        fruitNinjaButton GameZone_button = new fruitNinjaButton("Game Zone");
-        addMenuButtons(GameZone_button);
-
-    } private void createClosebutton(){
-        fruitNinjaButton close_button = new fruitNinjaButton("X");
-        addMenuButtons(close_button);
-
-    } private void createHelpbutton(){
-        fruitNinjaButton help_button = new fruitNinjaButton("Help");
-        addMenuButtons(help_button);
-
+        classicButton = new ImageView("View/resources/Classic.png");
+        classicButton.setLayoutX(50);
+        classicButton.setLayoutY(GAMEHIGHT-500);
+        classicButton.setOnMouseEntered(e->{ classicButton.setEffect(new DropShadow()); });
+        classicButton.setOnMouseExited(e->{ classicButton.setEffect(null); });
+        classicButton.setOnMouseClicked(e->{
+            System.out.println("Classic Game will open here");
+        });
+        mainpane.getChildren().add(classicButton);
     }
 
-
-
-
-
-
     private void createBackground(){
-        Image backgroundImage = new Image("view/resources/background-scene---dojo.png",1024,700,false,true);
+        Image backgroundImage = new Image("View/resources/backgroundLarge.png",GAMEWIDTH,GAMEHIGHT,false,true);
         BackgroundImage background = new BackgroundImage(backgroundImage, BackgroundRepeat.NO_REPEAT,BackgroundRepeat.NO_REPEAT , BackgroundPosition.DEFAULT,null);
         mainpane.setBackground(new Background(background));
+    }
+
+    private void creatLogo(){
+        ImageView logo = new ImageView("View/resources/logo.png");
+        logo.setLayoutX(GAMEWIDTH/2-180);
+        logo.setLayoutY(50);
+        logo.setOnMouseEntered(e->{ logo.setEffect(new DropShadow()); });
+        logo.setOnMouseExited(e->{ logo.setEffect(null); });
+        logo.setOnMouseClicked(e->{
+            System.out.println("Credit will open here");
+        });
+        mainpane.getChildren().add(logo);
     }
 
 }
